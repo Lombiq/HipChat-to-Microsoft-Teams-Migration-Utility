bf4804d237f9e0844dd8de91e123da85dc34ccc9
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2019-01-27 00:15:46 +0100

    Adding error handling for the case when a team's SharePoint site is still being provisioned
    --HG--
    branch : dev

--- Lombiq.HipChatToTeams/Services/AttachmentUploader.cs
+++ Lombiq.HipChatToTeams/Services/AttachmentUploader.cs
@@ -18,45 +18,58 @@ namespace Lombiq.HipChatToTeams.Services
 
             var itemRequest = importContext.GraphServiceClient.Groups[team.Id].Drive.Root.ItemWithPath(uploadPath);
 
-            using (var stream = IO.File.OpenRead(attachmentPath))
+            try
             {
-                if (fileSizeBytes / 1024 / 1024 > 3.6) // 4MB is the maximum for a simple upload, staying safe with 3.6.
+                using (var stream = IO.File.OpenRead(attachmentPath))
                 {
-                    var session = await itemRequest.CreateUploadSession().Request().PostAsync();
-                    var maxChunkSize = 320 * 4 * 1024;
-                    var provider = new ChunkedUploadProvider(session, importContext.GraphServiceClient, stream, maxChunkSize);
-
-                    var chunckRequests = provider.GetUploadChunkRequests();
-                    var exceptions = new List<Exception>();
-                    var readBuffer = new byte[maxChunkSize];
-                    DriveItem itemResult = null;
-
-                    foreach (var request in chunckRequests)
+                    if (fileSizeBytes / 1024 / 1024 > 3.6) // 4MB is the maximum for a simple upload, staying safe with 3.6.
                     {
-                        var result = await provider.GetChunkRequestResponseAsync(request, readBuffer, exceptions);
+                        var session = await itemRequest.CreateUploadSession().Request().PostAsync();
+                        var maxChunkSize = 320 * 4 * 1024;
+                        var provider = new ChunkedUploadProvider(session, importContext.GraphServiceClient, stream, maxChunkSize);
 
-                        if (result.UploadSucceeded)
+                        var chunckRequests = provider.GetUploadChunkRequests();
+                        var exceptions = new List<Exception>();
+                        var readBuffer = new byte[maxChunkSize];
+                        DriveItem itemResult = null;
+
+                        foreach (var request in chunckRequests)
                         {
-                            itemResult = result.ItemResponse;
-                        }
-                    }
+                            var result = await provider.GetChunkRequestResponseAsync(request, readBuffer, exceptions);
 
-                    // Retry if upload failed.
-                    if (itemResult == null && tryCount < 5)
-                    {
-                        await UploadFile(attachmentPath, team, channel, importContext, ++tryCount);
+                            if (result.UploadSucceeded)
+                            {
+                                itemResult = result.ItemResponse;
+                            }
+                        }
+
+                        // Retry if upload failed.
+                        if (itemResult == null && tryCount < 5)
+                        {
+                            TimestampedConsole.WriteLine($"Uploading the large attachment \"{attachmentPath}\" failed. Retrying.");
+                            return await UploadFile(attachmentPath, team, channel, importContext, ++tryCount);
+                        }
+                        else
+                        {
+                            throw new Exception($"The attachment \"{attachmentPath}\" couldn't be uploaded into the \"{channel.DisplayName}\" channel (\"{team.DisplayName}\" team) because upload failed repeatedly. You can try again as this might be just a temporary error. If the issue isn't resolved then delete the file so it's not uploaded.");
+                        }
                     }
                     else
                     {
-                        throw new Exception($"The attachment \"{attachmentPath}\" couldn't be uploaded into the \"{channel.DisplayName}\" channel (\"{team.DisplayName}\" team) because upload failed repeatedly. You can try again as this might be just a temporary error. If the issue isn't resolved then delete the file so it's not uploaded.");
+                        await itemRequest.Content.Request().PutAsync<DriveItem>(stream);
                     }
-                }
-                else
-                {
-                    await itemRequest.Content.Request().PutAsync<DriveItem>(stream);
-                }
 
-                return (await itemRequest.Request().GetAsync()).WebUrl;
+                    return (await itemRequest.Request().GetAsync()).WebUrl;
+                }
+            }
+            catch (ServiceException ex) when (ex.Message.Contains("Unable to provision resource."))
+            {
+                var waitMinutes = 5;
+                TimestampedConsole.WriteLine($"The team's SharePoint site is not yet set up (you can check this under the channel's Files tab) so attachments can't be uploaded. Waiting {waitMinutes} minutes.");
+                await Task.Delay(waitMinutes * 60000);
+                TimestampedConsole.WriteLine($"Retrying upload.");
+
+                return await UploadFile(attachmentPath, team, channel, importContext, ++tryCount);
             }
         }
     }
